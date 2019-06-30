9bac320e28c634fb4b546ee1d60eccee8aeb08c7
sirromdev <sirromdev@gmail.com>
2019-06-30 22:04:24 +0100

    making requests for blocknumber

--- src/eth/main.cljs
+++ src/eth/main.cljs
@@ -1,4 +1,24 @@
-(ns eth.main)
+(ns eth.main
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(def ganache-url "http://localhost:8545")
 
 (defn start [build]
   (println "using build" build))
+
+(defn initialize-provider []
+  ;; connect to blockchain
+  (println "connect to blockchain")
+  )
+
+(defn getBlockNumber
+  []
+  (go (let [response (<! (http/post ganache-url
+                                    {:json-params {:jsonrpc "2.0"
+                                                   :method "eth_blockNumber"
+                                                   :params []
+                                                   :id "3"}}))]
+        (println (:result (:body response)))))
+  )
